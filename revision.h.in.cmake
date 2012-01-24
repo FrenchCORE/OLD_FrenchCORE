9c31a28614e2720ec7a03815e93baf04581a403b
Atomic <julien.schmidt@wanadoo.fr>
2012-01-24 12:24:32 +0100

    [1] Initialisation des sources FrenchCORE.

---
+++ revision.h.in.cmake
@@ -0,0 +1,11 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _HASH                      "@rev_hash@"
+ #define _DATE                      "@rev_date@"
+ #define VER_COMPANYNAME_STR        "FrenchCORE Emulateur"
+ #define VER_LEGALCOPYRIGHT_STR     "(c) 2012 FrenchCORE"
+ #define VER_FILEVERSION            1
+ #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif // __REVISION_H__
